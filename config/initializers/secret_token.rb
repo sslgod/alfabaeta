5ee5809e47a5e9d9356d4fd614e317216e87e623
Dmitry <you@example.com>
2012-05-28 01:13:44 +0400

    adas

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Baeta::Application.config.secret_token = 'd21135997b43e106f3d6fcd59ac909a7221a95888a753ca0165f1ac5e6d0da2820c3eeaef08daf8bf1060d3f5f1167b2b5f5b05e9136e3d41cdf0ebe2e2d529e'
