910edb23d9f02e0435e6fbf7fb9c1c7d7c4cc634
Dmitry <you@example.com>
2012-05-29 19:41:00 +0400

    asdfasdfds

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,8 @@ Baeta::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = true
+  config.active_support.deprecation = :silence
+
 
   # Generate digests for assets URLs
   config.assets.digest = true
