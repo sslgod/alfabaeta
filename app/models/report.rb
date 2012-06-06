1aef3ca7296511b231b2a9531535ed900f77b116
Your Name <supp0rtgeeper@gmail.com>
2012-06-06 21:14:29 +0400

    sasas

--- app/models/report.rb
+++ app/models/report.rb
@@ -2,5 +2,5 @@ class Report < ActiveRecord::Base
 	attr_accessible :id, :comment, :site_id, :user_id, :file
 	belongs_to :user
  	belongs_to :site
- 	mount_uploader :image, FileUploader
+ 	mount_uploader :file, FileUploader
 end
