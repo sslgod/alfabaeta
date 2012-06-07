bb9e4d7194887d9db53ded36ab81f5c77a277718
Your Name <supp0rtgeeper@gmail.com>
2012-06-07 23:58:25 +0400

    asdfasdf

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,12 +10,7 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//
 //= require jquery
-//= require bootstrap
+//= require jquery_ujs
+//= require twitter/bootstrap
 //= require_tree .
-
-
- $("#openformotchet").click(function() {
- 	$("#openform").show();
- });
